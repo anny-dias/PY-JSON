cb1201404b7dea77a6cbcfcfe51c53fd50188189
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-24 09:46:06 -0300

    correcao

--- exercicio1.py
+++ exercicio1.py
@@ -25,4 +25,22 @@ Implemente um programa que leia este arquivo e gere um novo arquivo JSON no form
 }
 '''
 
+import json 
+
+with open('notas.csv', 'r', encoding='utf-8') as arquivo:
+    alunos = {}
+    conta_linha = 1
+    for linha in arquivo:
+        if conta_linha > 1:     #ignora a primeira linha do arquivo csv
+            lista = linha.split(';')
+
+            rm = lista[0]
+            nome = lista[1]
+            notas = [float(lista[2]), float(lista[3]), float(lista[4]), float(lista[5])]
+            alunos[rm] = {'nomes': nome, 'notas': notas}       #insere no dicionario
+        conta_linha += 1
+
+with open('notas.json', 'w', encoding='utf-8') as arquivo:
+    json.dump(alunos, arquivo, indent=4)
+
 
--- exercicio2.py
+++ exercicio2.py
@@ -18,3 +18,20 @@ Implemente um programa que a partir do arquivo indicado gere um arquivo JSON no 
  }
 }
 '''
+
+import json 
+
+with open('foods.csv', 'r', encoding='utf-8') as arquivo:
+    dicionario = {}
+    conta_linha = 1
+    for linha in arquivo:
+        if conta_linha > 1:                 #ignora a primeira linha do arquivo csv
+            lista = linha.split(';')
+
+            #armazena dados no dicionario
+            dicionario[lista[1]] = {'nome': lista[0], 'food': lista[2].replace('\n', '')}
+        conta_linha += 1
+
+#cria arquico json
+with open('food.json', 'w', encoding='utf-8') as arquivo:
+    json.dump(dicionario, arquivo, indent=4, ensure_ascii=False)
